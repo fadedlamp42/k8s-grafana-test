6b9c5c0f5cd9e401312919f4ea5127bd8ba36f0c
Peter Gomez <fadedlamp42@gmail.com>
2020-03-26 13:28:45 -0400

    util files: install-deps.sh installs grafana and prometheus with helm, graf-proxy.sh prints credentials and starts a proxy to the grafana dashboard

---
+++ install-deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#whereis check instead of -x; uncertain of default helm location
+helm_location=$(whereis helm)
+if [ "$helm_location" != "helm:" ]; then 
+    echo "helm found, installing prometheus + grafana"
+else
+    echo "helm not found, please see https://helm.sh/docs/intro/quickstart/"
+    exit
+fi
+
+helm install prometheus stable/prometheus
+helm install grafana stable/grafana
